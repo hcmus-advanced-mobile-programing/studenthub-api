6b1b059c2d0302fb1f85a88695606a0bdbb00278
LeThuyNga1606 <lethuynga1662002@gmail.com>
2024-03-27 09:19:59 +0700

    add data in tech stack enity

--- src/modules/techStack/teckStack.service.ts
+++ src/modules/techStack/teckStack.service.ts
@@ -1,14 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TechStack } from './techStack.entity';
 
 @Injectable()
-export class TechStackService {
+export class TechStackService implements OnModuleInit{
   constructor(
     @InjectRepository(TechStack)
     private readonly techStackRepository: Repository<TechStack>,
   ) {}
+
+  async onModuleInit() {
+    const techStacks = [
+      "Fullstack Engineer",
+      "Frontend Developer",
+      "Backend Developer"
+    ];
+
+    for (const techStackName of techStacks) {
+      const techStack = new TechStack();
+      techStack.name = techStackName;
+
+      await this.techStackRepository.save(techStack);
+    }
+  }
 
   async findAll(): Promise<TechStack[]> {
     return this.techStackRepository.find();
