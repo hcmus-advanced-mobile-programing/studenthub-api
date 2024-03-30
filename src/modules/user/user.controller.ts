e6bcd2c55bdaf1ac310d7dee069f6b4e96b27477
LeThuyNga1606 <lethuynga1662002@gmail.com>
2024-03-30 21:35:41 +0700

    fix API change password

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -22,8 +22,8 @@ export class UserController {
     return this.userService.getAllUser(args);
   }
 
-  @Put(':id')
-  @Auth()
+  @Put('changePassword')
+  @Auth() 
   changePassword(@Body() userChangePassDto: UserChangePassDto): Promise<void> {
     return this.userService.changePassword(userChangePassDto);
   }
