6c17b4177c62a7882fc2b91e689fba1277ee8605
Quách Vĩnh Khang <vinhkhangquach2002@gmail.com>
2024-04-03 15:22:32 +0700

    feat: add mq to event gateway
    add redis.yml for building redis, add bull message queue to handle message

--- src/modules/event/event.gateway.ts
+++ src/modules/event/event.gateway.ts
@@ -13,36 +13,77 @@ import {
 
 import { Server, Socket } from 'socket.io';
 
+import * as Queue from 'bull';
+
 @Injectable()
 @WebSocketGateway()
 export class EventGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private readonly jwtService: JwtService) {}
-  @WebSocketServer() server: Server;
+  @WebSocketServer() private server: Server;
+  private messageQueue: Queue.Queue;
+
+  constructor(private readonly jwtService: JwtService) {
+    this.messageQueue = new Queue('messageQueue');
+
+    this.messageQueue
+      .process(
+        (
+          job: Queue.Job<{
+            projectId: string;
+            content: string;
+            clientId: string;
+          }>, 
+          done
+        ) => {
+          const { projectId, content, clientId } = job.data;
+
+          this.server.to(projectId.toString()).emit('SEND_MESSAGE', { content });
+          this.sendNotificationToUidByProject(clientId, projectId.toString(), content);
+
+          done();
+        }
+      )
+      .catch((error) => {
+        throw new Error(error);
+      });
+
+    this.messageQueue.on('error', (error) => {
+      console.error('Error occurred in message queue: ', error);
+    });
+  }
 
   @SubscribeMessage('SEND_MESSAGE')
   handleMessage(@ConnectedSocket() client: Socket, @MessageBody() data): void {
-    const { project_id: projectId, content } = data;
+    try {
+      const { project_id: projectId, content } = data;
 
-    this.server.to(projectId).emit('SEND_MESSAGE', { content });
-
-    this.sendNotificationToUidByProject(client.id, projectId, content);
+      this.messageQueue.add({ projectId, content, clientId: client.id }).catch((error) => {
+        throw new Error(error);
+      });
+    } catch (error) {
+      console.error('Error occurred in message queue: ', error);
+      this.server.to(client.id).emit('ERROR', { content: 'Error occurred in message queue' });
+    }
   }
 
   sendNotificationToUidByProject(clientId: string, projectId: string, content: string): void {
-    const rooms = this.server.sockets.adapter.rooms;
-    const socketsInRooms = rooms.get(projectId);
+    try {
+      const rooms = this.server.sockets.adapter.rooms;
+      const socketsInRooms = rooms.get(projectId);
 
-    for (const socketId of socketsInRooms) {
-      if (socketId !== clientId) {
-        const socketDetail = this.server.sockets.sockets.get(socketId);
+      for (const socketId of socketsInRooms) {
+        if (socketId !== clientId) {
+          const socketDetail = this.server.sockets.sockets.get(socketId);
 
-        if (socketDetail) {
-          this.server.emit(`NOTI_${socketDetail.data.id}`, {
-            content,
-            noti: true,
-          });
+          if (socketDetail) {
+            this.server.emit(`NOTI_${socketDetail.data.id}`, {
+              content,
+              noti: true,
+            });
+          }
         }
       }
+    } catch (error) {
+      throw new Error(error);
     }
   }
 
@@ -55,8 +96,10 @@ export class EventGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
       const { email, id } = await this.jwtService.verify(socket.handshake.headers.authorization.split(' ')[1]);
       const { project_id } = socket.handshake.query;
 
+      console.log('Email: ', email, 'Id: ', id, 'Project id: ', project_id);
+
       socket.data = { email, id };
-      if (project_id) await socket.join(project_id);
+      if (project_id) await socket.join(project_id.toString());
     } catch (error) {
       console.log('Unauthorized connection');
       socket.disconnect();
