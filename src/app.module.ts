af65d2fea0322569d1df9ccc446491213b57e82b
quinxii <82009267+quinxii@users.noreply.github.com>
2024-04-06 16:10:06 +0700

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,6 @@ import { TypeOrmConfigService } from 'src/database/typeorm-config.service';
 import loggerConfig from 'src/logger/config';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { UserModule } from 'src/modules/user/user.module';
-import { MeetingRoomModule } from 'src/modules/meeting-room/meeting-room.module';
 import { HttRequestContextMiddleware } from 'src/shared/http-request-context/http-request-context.middleware';
 import { HttRequestContextModule } from 'src/shared/http-request-context/http-request-context.module';
 import { RequestIdHeaderMiddleware } from 'src/shared/middlewares/request-id-header.middleware';
@@ -45,7 +44,6 @@ import { ProposalModule } from 'src/modules/proposal/proposal.module';
     AuthModule,
     UserModule,
     ProposalModule,
-    MeetingRoomModule,
   ],
 })
 export class AppModule implements NestModule {
