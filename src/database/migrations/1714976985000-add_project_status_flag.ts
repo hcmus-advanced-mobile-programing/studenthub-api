2b3e3cb788b79163258794a71a5404cee84947e7
Tuan Lam <tuanlam16102002>
2024-05-06 13:37:48 +0700

    update migration

--- src/database/migrations/1714976985000-add_project_status_flag.ts
+++ src/database/migrations/1714976985000-add_project_status_flag.ts
@@ -4,30 +4,10 @@ export class AddProjectStatusFlag1714976985000 implements MigrationInterface {
   name = 'AddProjectStatusFlag1714976985000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "company" DROP CONSTRAINT "FK_c41a1d36702f2cd0403ce58d33a"`);
-    await queryRunner.query(`ALTER TABLE "company" DROP CONSTRAINT "REL_c41a1d36702f2cd0403ce58d33"`);
-    await queryRunner.query(`ALTER TABLE "company" DROP COLUMN "userId"`);
     await queryRunner.query(`ALTER TABLE "project" ADD "status" integer NOT NULL DEFAULT '0'`);
-    await queryRunner.query(`ALTER TABLE "notification" DROP CONSTRAINT "FK_bcacc62c929cc4881ec971b6791"`);
-    await queryRunner.query(`ALTER TABLE "notification" DROP CONSTRAINT "REL_bcacc62c929cc4881ec971b679"`);
-    await queryRunner.query(
-      `ALTER TABLE "notification" ADD CONSTRAINT "FK_bcacc62c929cc4881ec971b6791" FOREIGN KEY ("message_id") REFERENCES "message"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "notification" DROP CONSTRAINT "FK_bcacc62c929cc4881ec971b6791"`);
-    await queryRunner.query(
-      `ALTER TABLE "notification" ADD CONSTRAINT "REL_bcacc62c929cc4881ec971b679" UNIQUE ("message_id")`
-    );
-    await queryRunner.query(
-      `ALTER TABLE "notification" ADD CONSTRAINT "FK_bcacc62c929cc4881ec971b6791" FOREIGN KEY ("message_id") REFERENCES "message"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
-    );
     await queryRunner.query(`ALTER TABLE "project" DROP COLUMN "status"`);
-    await queryRunner.query(`ALTER TABLE "company" ADD "userId" integer`);
-    await queryRunner.query(`ALTER TABLE "company" ADD CONSTRAINT "REL_c41a1d36702f2cd0403ce58d33" UNIQUE ("userId")`);
-    await queryRunner.query(
-      `ALTER TABLE "company" ADD CONSTRAINT "FK_c41a1d36702f2cd0403ce58d33a" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
-    );
   }
 }
